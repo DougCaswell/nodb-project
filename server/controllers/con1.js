4ccc47200d745a13e276604253d0ad1c826d616c
DougCaswell <dougcaswell@gmail.com>
2018-11-16 09:25:48 -0700

    first commit

--- server/controllers/con1.js
+++ server/controllers/con1.js
@@ -1,6 +1,14 @@
+let bookList = [];
+let id = 0;
+
+
 module.exports={
     test: (req, res) => {
         console.log(req)
         res.status(200).send("Yeah!")
+    },
+    createBook: (req, res) => {
+        bookList.push({author: req.body.author, pages: req.body.pages, year: req.body.year, title: req.body.title})
+
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const controller = require('./controllers/con1.js')
 
 const app = express();
+
+app.get('/api/books', controller.test)
+
+
+
 const port = 3540;
-
-
-
-
 app.listen(port, () => console.log('I am running on port ' + port))
